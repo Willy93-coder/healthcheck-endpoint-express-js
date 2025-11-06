7eda7a6d00a62fda2fd051c71269acb6e7400ff2
Guillermo Cifre <guillecg13@gmail.com>
2025-11-06 20:37:47 +0100

    feat: add health router in server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,8 @@
-import express, { Application, Request, Response } from "express";
+import express, { Application } from "express";
+import healthRoutes from "./routes/healthRoutes";
+
 const app: Application = express();
 
-// Health check endpoint (app)
-app.get("/health", (_req: Request, res: Response) => {
-  res.status(200).json({
-    status: "OK",
-    timestamp: new Date().toISOString(),
-    service: "Inventory control API",
-  });
-});
+app.use("/api", healthRoutes);
 
 export { app };
