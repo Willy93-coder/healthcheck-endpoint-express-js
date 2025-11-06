d88b3a10a5e34106f326a4a5f478c39cb6bb39fd
Guillermo Cifre <guillecg13@gmail.com>
2025-11-06 17:43:21 +0100

    chore: refactor index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,5 @@
-import express, { Application, Request, Response } from "express";
+import { app } from "./server";
 import { env } from "../env";
-
-const app: Application = express();
-
-app.get("/", (req: Request, res: Response) => {
-  res.json({ message: "Hello, world" });
-});
 
 app.listen(env.PORT, () => {
   console.log(`Server is running on port ${env.PORT}`);
