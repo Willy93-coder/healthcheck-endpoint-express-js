bc6832aeacabd98e228078763436d5df67768d55
Guillermo Cifre <guillecg13@gmail.com>
2025-11-06 20:42:02 +0100

    chore: enhance db

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,6 +1,10 @@
 import mongoose from "mongoose";
 import { env } from "../../env";
 
-const connectDB = async () => {
-  await mongoose.connect(env.MONGO_URI);
+const db = {
+  connect: async () => {
+    await mongoose.connect(env.MONGO_URI);
+  },
 };
+
+export { db };
