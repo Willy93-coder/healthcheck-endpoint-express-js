d17e5a8de2d8216b676f9303c092259de36e9ce1
Guillermo Cifre <guillecg13@gmail.com>
2025-12-03 19:42:54 +0100

    feat: add check states functions in MongoState

--- src/db/mongoState.ts
+++ src/db/mongoState.ts
@@ -6,5 +6,10 @@ const MongoState = {
   set: (next: MongodbState): void => {
     MongoState.state = next;
   },
-  isReady: () => MongoState.state === "ready",
+  isReady: (): boolean => MongoState.state === "ready",
+  isConnecting: (): boolean => MongoState.state === "connecting",
+  isDisconnecting: (): boolean => MongoState.state === "disconnected",
+  isError: (): boolean => MongoState.state === "error",
 };
+
+export { MongoState };
